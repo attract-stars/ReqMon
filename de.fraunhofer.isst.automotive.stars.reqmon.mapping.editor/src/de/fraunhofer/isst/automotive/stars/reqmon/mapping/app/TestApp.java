ed035d76cd82890799d406d0984dccf85f5fd297
Malte Mauritz <malte.mauritz@isst.fraunhofer.de>
2019-03-19 16:44:01 +0100

    renamed project and fixed extension

---
+++ de.fraunhofer.isst.automotive.stars.reqmon.mapping.editor/src/de/fraunhofer/isst/automotive/stars/reqmon/mapping/app/TestApp.java
@@ -0,0 +1,48 @@
+package de.fraunhofer.isst.automotive.stars.reqmon.mapping.app;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class TestApp {
+
+	protected Shell shell;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			TestApp window = new TestApp();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+
+	}
+
+}
