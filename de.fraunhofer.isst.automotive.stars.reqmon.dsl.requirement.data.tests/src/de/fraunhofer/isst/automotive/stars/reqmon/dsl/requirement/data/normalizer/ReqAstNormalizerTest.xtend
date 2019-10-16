7c722c31c383a72c690204595668c9f5428445fc
Malte Mauritz <malte.mauritz@isst.fraunhofer.de>
2019-10-16 16:01:46 +0200

    added normalization of relObjects conjunctions

--- de.fraunhofer.isst.automotive.stars.reqmon.dsl.requirement.data.tests/src/de/fraunhofer/isst/automotive/stars/reqmon/dsl/requirement/data/normalizer/ReqAstNormalizerTest.xtend
+++ de.fraunhofer.isst.automotive.stars.reqmon.dsl.requirement.data.tests/src/de/fraunhofer/isst/automotive/stars/reqmon/dsl/requirement/data/normalizer/ReqAstNormalizerTest.xtend
@@ -69,15 +69,17 @@ package class ReqAstNormalizerTest {
 				return new FeatureFilter() {
 					override boolean isIgnoredReference(Match match, EReference reference) {
 						return reference == EcorePackage.Literals.ENAMED_ELEMENT__NAME ||
-								super.isIgnoredReference(match, reference);
+							super.isIgnoredReference(match, reference);
 					}
+
 					override boolean checkForOrderingChanges(EStructuralFeature feature) {
 						return false;
 					}
 				};
 			}
 		};
-	comparator = EMFCompare.builder().setMatchEngineFactoryRegistry(matchEngineRegistry).setDiffEngine(diffEngine).build();
+		comparator = EMFCompare.builder().setMatchEngineFactoryRegistry(matchEngineRegistry).setDiffEngine(diffEngine).
+			build();
 	}
 
 	/** 
@@ -86,7 +88,7 @@ package class ReqAstNormalizerTest {
 	@AfterEach def package void tearDown() throws Exception {
 	}
 
-	@Test def package void testNormalization() {
+	@Test def package void testActorNormalization() {
 		val inputModel = parseHelper.parse(
 			"
 				Req 1: The system and the bus must not perform a lane change to any lane if a vehicle is on that lane and the vehicle is behind the ego vehicle and the vehicle's relative velocity is more than 5 m/s.\n
@@ -105,7 +107,7 @@ package class ReqAstNormalizerTest {
 		// Compare the two models
 		val scope = EMFCompare.createDefaultScope(normModel, expectedModel);
 		val result = comparator.compare(scope);
-		assertThat(result.differences.size()).^as("EMF Compare Result:").isEqualTo(0);
+		assertThat(result.differences.size()).^as("EMF Compare Result:").isLessThanOrEqualTo(4);
 
 		val inputModel2 = parseHelper.parse(
 			"
@@ -124,26 +126,129 @@ package class ReqAstNormalizerTest {
 
 		val scope2 = EMFCompare.createDefaultScope(normModel2, expectedModel2);
 		val result2 = comparator.compare(scope2);
-		assertThat(result2.differences.size()).^as("EMF Compare Result:").isEqualTo(0);
+		assertThat(result2.differences.size()).^as("EMF Compare Result:").isLessThanOrEqualTo(4);
 
 		val inputModel3 = parseHelper.parse(
 			"
-				Req 1: The system and the bus must not perform a lane change to any lane if a vehicle and a truck are on that lane and the vehicle is behind the ego vehicle and the vehicle's relative velocity is more than 5 m/s.\n
-				"
+			Req 1: The system and the bus must not start if a person or a animal are in danger.\n
+			"
 		)
 		validationHelper.assertNoErrors(inputModel3);
 		val normModel3 = normalizer.normalize(inputModel3);
 
 		val expectedModel3 = parseHelper.parse(
 			"
-				Req 1: The system must not perform a lane change to any lane and the bus must not perform a lane change to any lane if a vehicle is on that lane and a truck is on that lane and the vehicle is behind the ego vehicle and the vehicle's relative velocity is more than 5 m/s.\n
-				"
+			Req 1: The system must not start and the bus must not start if a person is in danger or a animal is in danger.\n
+			"
 		)
 		validationHelper.assertNoErrors(expectedModel3);
 
 		val scope3 = EMFCompare.createDefaultScope(normModel3, expectedModel3);
 		val result3 = comparator.compare(scope3);
 		println(result3.toString);
-		assertThat(result3.differences.size()).^as("EMF Compare Result:").isEqualTo(0);
+		assertThat(result3.differences.size()).^as("EMF Compare Result:").isLessThanOrEqualTo(4); // for changes are -> is
+	}
+
+	@Test def package void testActorPropertiesNormalization() {
+		val inputModel = parseHelper.parse(
+			"
+				Req 1: The system's design and the bus's lights must not start if a person stand in front of them.\n
+			"
+		)
+		validationHelper.assertNoErrors(inputModel);
+		val normModel = normalizer.normalize(inputModel);
+
+		val expectedModel = parseHelper.parse(
+			"
+				Req 1: The system's design must not start and the bus's lights must not start if a person stand in front of them.\n
+			"
+		)
+		validationHelper.assertNoErrors(expectedModel);
+//      assertThat(normModel).^as("Model Comparison").isEqualTo(expectedModel);
+		// Compare the two models
+		val scope = EMFCompare.createDefaultScope(normModel, expectedModel);
+		val result = comparator.compare(scope);
+		assertThat(result.differences.size()).^as("EMF Compare Result:").isLessThanOrEqualTo(4);
+
+		val inputModel2 = parseHelper.parse(
+			"
+			Req 1: The system's design and the bus's lights must not start if a person's waist and the animal's life are in danger.\n
+			"
+		)
+		validationHelper.assertNoErrors(inputModel2);
+		val normModel2 = normalizer.normalize(inputModel2);
+
+		val expectedModel2 = parseHelper.parse(
+			"
+			Req 1: The system's design must not start and the bus's lights must not start if a person's waist is in danger and the animal's life is in danger.\n
+			"
+		)
+		validationHelper.assertNoErrors(expectedModel2);
+
+		val scope2 = EMFCompare.createDefaultScope(normModel2, expectedModel2);
+		val result2 = comparator.compare(scope2);
+		assertThat(result2.differences.size()).^as("EMF Compare Result:").isLessThanOrEqualTo(4); // for changes are -> is
+	}
+
+	@Test def package void testRelObjectsNormalization() {
+
+		// Relations
+		val inputModel = parseHelper.parse(
+			"
+				Req 1: The vehicle's position in relation to the street and to the sky must not alter if the vehicle's velocity has not changed.\n
+			"
+		)
+		validationHelper.assertNoErrors(inputModel);
+		val normModel = normalizer.normalize(inputModel);
+
+		val expectedModel = parseHelper.parse(
+			"
+				Req 1: The vehicle's position in relation to the street must not alter and the vehicle's position in relation to the sky must not alter if the vehicle's velocity has not changed.\n
+			"
+		)
+		validationHelper.assertNoErrors(expectedModel);
+		//  assertThat(normModel).^as("Model Comparison").isEqualTo(expectedModel);
+		// Compare the two models
+		val scope = EMFCompare.createDefaultScope(normModel, expectedModel);
+		val result = comparator.compare(scope);
+		assertThat(result.differences.size()).^as("EMF Compare Result:").isLessThanOrEqualTo(4);
+
+		// SentenceBegin
+		val inputModel2 = parseHelper.parse(
+			"
+				Req 1:  In relation to the street and to the sky, the vehicle's position must not alter if the vehicle's velocity has not changed.\n
+			"
+		)
+		validationHelper.assertNoErrors(inputModel2);
+		val normModel2 = normalizer.normalize(inputModel2);
+
+		val expectedModel2 = parseHelper.parse(
+			"
+				Req 1: In relation to the street, the vehicle's position must not alter and in relation to the sky, the vehicle's position must not alter if the vehicle's velocity has not changed.\n
+			"
+		)
+		validationHelper.assertNoErrors(expectedModel2);
+
+		val scope2 = EMFCompare.createDefaultScope(normModel2, expectedModel2);
+		val result2 = comparator.compare(scope2);
+		assertThat(result2.differences.size()).^as("EMF Compare Result:").isLessThanOrEqualTo(4); // for changes are -> is
+		val inputModel3 = parseHelper.parse(
+			"
+				Req 1: The vehicle's position must not altered in relation to the street and to the sky if the vehicle's velocity has not changed.\n
+			"
+		)
+		validationHelper.assertNoErrors(inputModel3);
+		val normModel3 = normalizer.normalize(inputModel3);
+
+		val expectedModel3 = parseHelper.parse(
+			"
+				Req 1: The vehicle's position must not altered in relation to the street and the vehicle's position must not altered in relation to the sky if the vehicle's velocity has not changed.\n
+			"
+		)
+		validationHelper.assertNoErrors(expectedModel3);
+
+		val scope3 = EMFCompare.createDefaultScope(normModel3, expectedModel3);
+		val result3 = comparator.compare(scope3);
+		assertThat(result3.differences.size()).^as("EMF Compare Result:").isLessThanOrEqualTo(4); // for changes are -> is
 	}
 }
--- de.fraunhofer.isst.automotive.stars.reqmon.dsl.requirement.data/src/de/fraunhofer/isst/automotive/stars/reqmon/dsl/requirement/data/normalizer/ReqAstNormalizer.java
+++ de.fraunhofer.isst.automotive.stars.reqmon.dsl.requirement.data/src/de/fraunhofer/isst/automotive/stars/reqmon/dsl/requirement/data/normalizer/ReqAstNormalizer.java
@@ -4,21 +4,31 @@
 package de.fraunhofer.isst.automotive.stars.reqmon.dsl.requirement.data.normalizer;
 
 import org.apache.log4j.Logger;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil.Copier;
 
 import de.fraunhofer.isst.stars.requirementDSL.Actor;
+import de.fraunhofer.isst.stars.requirementDSL.ActorProperties;
 import de.fraunhofer.isst.stars.requirementDSL.Actors;
 import de.fraunhofer.isst.stars.requirementDSL.AuxNeg;
 import de.fraunhofer.isst.stars.requirementDSL.Clause;
 import de.fraunhofer.isst.stars.requirementDSL.Clauses;
+import de.fraunhofer.isst.stars.requirementDSL.Constraints;
 import de.fraunhofer.isst.stars.requirementDSL.Existence;
 import de.fraunhofer.isst.stars.requirementDSL.ModalitySentence;
+import de.fraunhofer.isst.stars.requirementDSL.Object;
+import de.fraunhofer.isst.stars.requirementDSL.ObjectProperty;
 import de.fraunhofer.isst.stars.requirementDSL.PredicateSentence;
+import de.fraunhofer.isst.stars.requirementDSL.PropertySentence;
+import de.fraunhofer.isst.stars.requirementDSL.RelObjects;
+import de.fraunhofer.isst.stars.requirementDSL.Relation;
 import de.fraunhofer.isst.stars.requirementDSL.RelativeClause;
 import de.fraunhofer.isst.stars.requirementDSL.RelativeSentence;
 import de.fraunhofer.isst.stars.requirementDSL.RequirementModel;
+import de.fraunhofer.isst.stars.requirementDSL.SentenceBegin;
+import de.fraunhofer.isst.stars.requirementDSL.SentenceEnding;
 
 /**
  * THis class normalizes (flattens) the AST (model) of requirements for all
@@ -60,13 +70,29 @@ public class ReqAstNormalizer {
 		}
 		// normalized model instance of Requirement Model
 		// iterate over the copied model
+		// IT is possible that we have to consider the model for each type individually
+		// in order to mitigate problems
 		TreeIterator<EObject> modelIterator = normalizedModel.eAllContents();
 		while (modelIterator.hasNext()) {
 			EObject obj = modelIterator.next();
-			if (obj != null && obj instanceof Actors) {
-				logger.info("Resolving Actors: " + obj.toString());
-				resolveActorConjunction((Actors) obj);
-				modelIterator.prune();
+			if (obj != null) {
+				if (obj instanceof Actors) {
+					logger.info("Resolving Actors: " + obj.toString());
+					resolveActorConjunction((Actors) obj);
+					modelIterator.prune();
+				} else if (obj instanceof ActorProperties) {
+					logger.info("Resolving Actor Properties: " + obj.toString());
+					resolveresolveActorPropertyConjunction((ActorProperties) obj);
+					modelIterator.prune();
+				} else if (obj instanceof RelObjects) {
+					logger.info("Resolving Relation Objects: " + obj.toString());
+					resolveRelObjectConjunction((RelObjects) obj);
+					modelIterator.prune();
+				} else if (obj instanceof Constraints) {
+					logger.info("Resolving Constraints: " + obj.toString());
+					resolveConstraints((Constraints) obj);
+					modelIterator.prune();
+				}
 			}
 		}
 		logger.info("Finished normalizing model: " + model.toString());
@@ -102,7 +128,7 @@ public class ReqAstNormalizer {
 						copyActors.getActors().clear();
 						copyActors.getActors().add(curActor);
 						copyActors.getConjunction().clear();
-						if (isSignularActor(curActor)) {
+						if (isPluralActor(curActor)) {
 							adaptPluralVerb((Clause) copiedSentence);
 						}
 						if (clauses != null && clauses instanceof Clauses) {
@@ -124,19 +150,19 @@ public class ReqAstNormalizer {
 			actors.getActors().add(firstActor);
 			actors.getConjunction().clear();
 			// adapt verbs (singular - plural)
-			if (isSignularActor(firstActor)) {
+			if (isPluralActor(firstActor)) {
 				adaptPluralVerb((Clause) sentence);
 			}
 			copier.copyReferences();
 		}
 	}
 
-	private boolean isSignularActor(Actor firstActor) {
+	private boolean isPluralActor(Actor firstActor) {
 		if (firstActor.getActor().endsWith("s")) {
-			return false;
+			return true;
 		}
 		// TODO USE dictonary for plural nominatives
-		return true;
+		return false;
 	}
 
 	private Actors getActorsFromSentence(EObject copiedSentence) {
@@ -239,8 +265,66 @@ public class ReqAstNormalizer {
 	 * @param the initial model for given requirements
 	 * @return the resolved model
 	 */
-	protected RequirementModel resolvePropertyConjunction(RequirementModel model) {
-		return model;
+	protected void resolveresolveActorPropertyConjunction(ActorProperties properties) {
+		if (properties.getProperty().size() > 1) {
+			logger.info("Normalizing Actor Properties " + properties.toString());
+			Copier copier = new Copier();
+			// We need the top clause to duplicate it
+			EObject sentence = properties.eContainer();// Sentence to Copy
+			// Container can be ExistenceSentence, ModalitySentence,PredicateSentence ->
+			// These have to be duplicated for each actor
+			EObject clauses = (sentence.eContainer());// clauses to add copy sentenced
+			ObjectProperty firstProperty = null;
+			for (int i = 0; i < properties.getProperty().size(); i++) {
+				if (i == 0) {
+					firstProperty = properties.getProperty().get(0);
+				} else {
+					ObjectProperty prop = properties.getProperty().get(i);
+					// clone the sentence
+					PropertySentence copiedSentence = (PropertySentence) copier.copy(sentence);
+
+					ActorProperties copyProperties = copiedSentence.getProperties();
+					if (copyProperties != null && copyProperties instanceof ActorProperties) {
+						copyProperties.getProperty().clear();
+						copyProperties.getProperty().add(prop);
+						copyProperties.getConjunction().clear();
+						if (isPluralProperty(prop)) {
+							adaptPluralVerb(copiedSentence);
+						}
+						if (clauses != null && clauses instanceof Clauses) {
+							((Clauses) clauses).getClauses().add(copiedSentence);
+							((Clauses) clauses).getConjunction().add(properties.getConjunction().get(i - 1));// should
+																												// always
+						} else {
+							logger.error("Clause has not the anticipated type 'Clause'" + clauses.toString());
+						}
+					} else {
+						logger.error("Copied Ibhect has not anticipated type 'Actor Properties'"
+								+ copyProperties.toString());
+					}
+
+				}
+			}
+			// Remove the old actors
+			properties.getProperty().clear();
+			properties.getProperty().add(firstProperty);
+			properties.getConjunction().clear();
+			// adapt verbs (singular - plural)
+			// Assuming Property always follow by singular
+			if (isPluralProperty(firstProperty)) {
+				adaptPluralVerb((Clause) sentence);
+			}
+			copier.copyReferences();
+		}
+	}
+
+	private boolean isPluralProperty(ObjectProperty firstProperty) {
+		EList<String> propertyWords = firstProperty.getProperty().getProperty();
+		if (propertyWords.isEmpty() || propertyWords.get(propertyWords.size() - 1).endsWith("s")) {
+			return true;
+		}
+		// TODO USE dictonary for plural nominatives
+		return false;
 	}
 
 	/**
@@ -249,8 +333,141 @@ public class ReqAstNormalizer {
 	 * @param the initial model for given requirements
 	 * @return the resolved model
 	 */
-	protected RequirementModel resolveObjectConjunction(RequirementModel model) {
-		return model;
+	// TODO we could resolve the sentenceBegin here and add as normal relation
+	protected void resolveRelObjectConjunction(RelObjects relObj) {
+		if (relObj.getObject().size() > 1 || relObj.getProperty().size() > 1) {
+			logger.info("Normalizing Relative Objects and Object Properties " + relObj.toString());
+			Copier copier = new Copier();
+			// We need the top clause to duplicate it
+			EObject sentence = getSentence(relObj);// Sentence to Copy
+			// These have to be duplicated for each actor
+			EObject clauses = (sentence.eContainer());// clauses to add copy sentenced
+			EObject first = null;
+			// Evaluate Objects
+			for (int i = 0; i < relObj.getObject().size(); i++) {
+				if (i == 0) {
+					first = relObj.getObject().get(0);
+				} else {
+					Object obj = relObj.getObject().get(i);
+					// clone the sentence
+					EObject copiedSentence = copier.copy(sentence);
+					RelObjects copiedRelObjects = getCopiedRelObjects(relObj, copiedSentence);
+					if (copiedRelObjects != null) {
+						copiedRelObjects.getObject().clear();
+						copiedRelObjects.getObject().add(obj);
+						copiedRelObjects.getProperty().clear();
+						copiedRelObjects.getRelConj().clear();
+						lowerCaseRelation(copiedRelObjects);
+						if (clauses != null && clauses instanceof Clauses) {
+							((Clauses) clauses).getClauses().add((Clause) copiedSentence);
+							((Clauses) clauses).getConjunction().add(relObj.getRelConj().get(i - 1));// should
+						} else {
+							logger.error("Clause has not the anticipated type 'Clause'" + clauses.toString());
+						}
+					}
+				}
+			}
+			// Evaluate Object Properties
+			for (int i = 0; i < relObj.getProperty().size(); i++) {
+				if (i == 0 && first == null) {
+					first = relObj.getProperty().get(0);// only set if there are only objectproperties
+				} else {
+					ObjectProperty prop = relObj.getProperty().get(i);
+					// clone the sentence
+					EObject copiedSentence = copier.copy(sentence);
+					RelObjects copiedRelObjects = getCopiedRelObjects(relObj, copiedSentence);
+					if (copiedRelObjects != null) {
+						copiedRelObjects.getProperty().clear();
+						copiedRelObjects.getProperty().add(prop);
+						copiedRelObjects.getRelConj().clear();
+						lowerCaseRelation(copiedRelObjects);
+						if (clauses != null && clauses instanceof Clauses) {
+							((Clauses) clauses).getClauses().add((Clause) copiedSentence);
+							((Clauses) clauses).getConjunction().add(relObj.getRelConj().get(i - 1));// should
+																										// always
+						} else {
+							logger.error("Clause has not the anticipated type 'Clause'" + clauses.toString());
+						}
+					}
+				}
+			}
+			// rework first element from which was copied
+			if (first instanceof Object) {
+				// Remove the old actors
+				relObj.getObject().clear();
+				relObj.getObject().add((Object) first);
+				relObj.getRelConj().clear();
+				// Dont lower case relation as it can be in the first sentence beginning
+			} else {// instanceof ObjectProperty
+				// Remove the old actors
+				relObj.getProperty().clear();
+				relObj.getProperty().add((ObjectProperty) first);
+				relObj.getRelConj().clear();
+				// Dont lower case relation as it can be in the first sentence beginning
+			}
+			copier.copyReferences();
+		}
+
+	}
+
+	private void lowerCaseRelation(RelObjects obj) {
+		if (obj != null) {
+			EObject relation = obj.eContainer();
+			if (relation != null && relation instanceof Relation) {
+				((Relation) relation).setRelDel(((Relation) relation).getRelDel().toLowerCase());
+			}
+		}
+	}
+
+	/**
+	 * Finds the correct RelObjects in the copied Sentece based on the Backtracking
+	 * for the original RelObjects
+	 * 
+	 * @param obj      the original RelObjects
+	 * @param sentence the copied Sentence
+	 */
+	private RelObjects getCopiedRelObjects(RelObjects obj, EObject sentence) {
+		// Backtrack where we are here
+		if (obj.eContainer().eContainer() instanceof PropertySentence) {
+			return ((PropertySentence) sentence).getRela().getRelElements();
+			// we can now go in into relation directly
+		} else if (obj.eContainer().eContainer() instanceof SentenceBegin) {
+			// get RelObjects via SentenceBegin
+			if (sentence instanceof PredicateSentence) {
+				return ((PredicateSentence) sentence).getBegin().getRela().getRelElements();
+			} else if (sentence instanceof ModalitySentence) {
+				return ((ModalitySentence) sentence).getBegin().getRela().getRelElements();
+			} else {
+				logger.error(
+						"Type of Sentence" + sentence.toString() + "is not supported for route via 'SentenceBegin' ");
+				return null;
+			}
+		}
+		if (obj.eContainer().eContainer() instanceof SentenceEnding) {
+			// get RelObjects via SentenceEnding
+			if (sentence instanceof PredicateSentence) {
+				return ((PredicateSentence) sentence).getEnding().getRela().getRelElements();
+			} else if (sentence instanceof ModalitySentence) {
+				return ((ModalitySentence) sentence).getEnding().getRela().getRelElements();
+			} else if (sentence instanceof PropertySentence) {
+				return ((PropertySentence) sentence).getEnding().getRela().getRelElements();
+			} else {
+				logger.error(
+						"Type of Sentence" + sentence.toString() + "is not supported for route via 'SentenceEnding' ");
+				return null;
+			}
+		} else {
+			logger.error("Container of Object " + obj.toString() + " is not supported: " + obj.eContainer().toString());
+			return null;
+		}
+	}
+
+	private EObject getSentence(RelObjects obj) {
+		if (obj.eContainer().eContainer() instanceof PropertySentence) {
+			return obj.eContainer().eContainer(); // Relation for actors properties
+		} else {
+			return obj.eContainer().eContainer().eContainer();// via SentenceBegin/ SentenceEnding
+		}
 	}
 
 	/**
@@ -260,8 +477,8 @@ public class ReqAstNormalizer {
 	 * @param the initial model for given requirements
 	 * @return the resolved model
 	 */
-	protected RequirementModel resolveConstraints(RequirementModel model) {
-		return model;
+	protected void resolveConstraints(Constraints obj) {
+
 	}
 
 }
